604ecac64cfe2e1341b573905f3f04d9abdf9b75
Hossien Mehdikhah <hossien_mehdikhah@hotmail.com>
2024-01-12 16:38:28 +0330

    Add project files.

---
+++ CollectionManager/WinUI/UserControllers/CollectionItemUserControl.xaml.cs
@@ -0,0 +1,10 @@
+using Microsoft.UI.Xaml.Controls;
+namespace CollectionManager.WinUI.UserControllers;
+
+public sealed partial class CollectionItemUserControl : UserControl
+{
+    public CollectionItemUserControl()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ CollectionManager/WinUI/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WinUI.ViewModels;
+
+public partial class MainViewModel : ObservableRecipient
+{
+    public MainViewModel()
+    {
+    }
+    public IList<Uri> GaleryLinkList
+    {
+        get ;
+    }
+}
